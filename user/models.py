2852480fe51918c726f5cb93d751995f7cdbcb3b
abdallhfattah <abdallh.fattah33@gmail.com>
2025-03-09 17:16:55 +0200

    app: users model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+class User(AbstractUser):
+    phone = PhoneNumberField()
+    bio = models.TextField(verbose_name="Bio", blank=True, null=True)
+    dob = models.DateField(verbose_name="Date of Birth", blank=True, null=True)
+    location = models.CharField(verbose_name="Location", max_length=255, blank=True, null=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.username  # Ensures a meaningful string representation
