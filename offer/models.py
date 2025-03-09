595b4220b403c7ffb4747062b9a280836db94ddb
abdallhfattah <abdallh.fattah33@gmail.com>
2025-03-09 17:51:29 +0200

    app: offer model

--- offer/models.py
+++ offer/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+
+class Offer(models.Model):
+    class OfferStatus(Enum):
+        PENDING = "PENDING"
+        ACCEPTED = "ACCEPTED"
+        REJECTED = "REJECTED"
+        SOLD = "SOLD"
+
+    post = models.ForeignKey(
+        "post.Post", on_delete=models.CASCADE, related_name="offers"
+    )
+    title = models.CharField(max_length=150)
+    lower_bound = models.DecimalField(max_digits=10, decimal_places=2)
+    upper_bound = models.DecimalField(max_digits=10, decimal_places=2)
+    sold_price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
+
+    status = models.CharField(
+        max_length=10,
+        choices=[
+            (status.value, status.name) for status in OfferStatus
+        ],
+        default=OfferStatus.PENDING.value,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.status}"
