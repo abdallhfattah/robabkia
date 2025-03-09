6433054874c904e4877df518ce5effac25d38094
abdallhfattah <abdallh.fattah33@gmail.com>
2025-03-09 17:17:25 +0200

    app: post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
