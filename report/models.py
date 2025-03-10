cfbcef5140e0e459aaa590be5a9bda68c2b14fa5
mohamedRajab0 <elzaimmohamed@gmail.com>
2025-03-10 12:03:22 +0200

    init report model

---
+++ report/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+# Create your models here.
+
+class Report(models.Model):
+    '''Report Model '''
+    
+    reporter = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    reported = GenericForeignKey('content_type', 'object_id')
+    reason = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.reported.username
